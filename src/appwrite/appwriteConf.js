e28694dc7fe3b014c9671cb0f4cd0e182e49fa02
William <captain-donut@hotmail.com>
2023-02-13 10:54:09 +0100

    Switched to HTTPS protocol for backend parts

--- src/appwrite/appwriteConf.js
+++ src/appwrite/appwriteConf.js
@@ -7,5 +7,5 @@ export const account = new Account(client);
 export const databases = new Databases(client, "63dc0ed016dccb1ebc1a") //database id: 63dc0eaf483ed667f54a, collection id: 63dc0ed016dccb1ebc1a
 
 client
-    .setEndpoint('http://45.153.187.162:234/v1')
+    .setEndpoint('https://45.153.187.162/v1')
     .setProject('63dc0c0837d79bb0f999');
