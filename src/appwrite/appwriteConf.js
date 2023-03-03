7a2aaa0f16bc7ccc4762ede1cdc124d6eab07af1
William <captain-donut@hotmail.com>
2023-03-03 16:00:28 +0100

    Change database

--- src/appwrite/appwriteConf.js
+++ src/appwrite/appwriteConf.js
@@ -4,7 +4,7 @@ import {Client, Account, Databases} from 'appwrite'
 
 const client = new Client();
 export const account = new Account(client);
-export const databases = new Databases(client, "63dc0ed016dccb1ebc1a") //database id: 63dc0eaf483ed667f54a, collection id: 63dc0ed016dccb1ebc1a
+export const databases = new Databases(client, "64020b71e5bf81bf875a") //database id: 64020b470c9c427c3b5b, collection id: 64020b71e5bf81bf875a
 
 client
     .setEndpoint('https://appwrite.donut.zone/v1')
