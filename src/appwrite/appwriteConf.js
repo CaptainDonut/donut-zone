70b87157f561278c71826d238f8d740e46a3306d
William Gullin <captain-donut@hotmail.com>
2023-03-04 13:05:24 +0100

    added custom domain

--- src/appwrite/appwriteConf.js
+++ src/appwrite/appwriteConf.js
@@ -7,5 +7,5 @@ export const account = new Account(client);
 export const databases = new Databases(client, "64020b71e5bf81bf875a") //database id: 64020b470c9c427c3b5b, collection id: 64020b71e5bf81bf875a
 
 client
-    .setEndpoint('https://45.153.187.162/v1')
+    .setEndpoint('https://appwrite.donut.zone/v1')
     .setProject('6402095141b7d552c3c0')
